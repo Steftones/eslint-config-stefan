46a08faf70f197685e10cc852d7164f2e7c32e4c
T-BX\Stefan.Sokolowki <stefan.sokolowski@darwin.com>
2024-06-17 10:40:10 +0100

    feat(1.2.7): adding stylelint react rules

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,9 +1,12 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss'],
   plugins: ['stylelint-selector-bem-pattern', 'stylelint-scss'],
-  ignoreFiles: ['node_modules/**', 'coverage/**', 'dist/**'],
+  ignoreFiles: ['node_modules/**', 'coverage/**', 'dist/**', 'build/**'],
   rules: {
-    'selector-class-pattern': null, // camel case preferred for React
+    // camel case preferred for React
+    'selector-id-pattern': null,
+    'selector-class-pattern': null,
+
     'keyframes-name-pattern': null,
     'plugin/selector-bem-pattern': {
       componentName: '[A-Z]+',
